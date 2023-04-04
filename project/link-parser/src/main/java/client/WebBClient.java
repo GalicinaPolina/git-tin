7b2bea42e1b34f6879cc504ee77c6e680815c2e4
GalicinaPolina <125124988+GalicinaPolina@users.noreply.github.com>
2023-04-04 22:50:53 +0500

    Исправила WebBClient

--- project/link-parser/src/main/java/client/WebBClient.java
+++ project/link-parser/src/main/java/client/WebBClient.java
@@ -4,21 +4,15 @@ import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.reactive.ClientHttpConnector;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import java.net.http.HttpClient;
-import java.util.logging.Logger;
 
 @Component
-public class WebClient {
+public class WebBClient {
+
     @Bean
     public static ClientResponse createWebClient(ClientRequest request)
     {
@@ -32,4 +26,3 @@ public class WebClient {
 
         return response;
     }
-}
